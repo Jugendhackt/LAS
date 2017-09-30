d5de46c7a16907efd10e4d27d988ff2dcf8aa5b1
sevenmaster <seven-master@hotmail.de>
2017-09-30 22:14:19 +0200

    zurück nach speichern

--- jumbotron/Chat/ChatScript.js
+++ jumbotron/Chat/ChatScript.js
@@ -17,7 +17,10 @@ document.getElementById ("TextInput").addEventListener ("keyup", TextKeyUp);
 var possibleMehrStuff =
   [
     "Ich habe hier paar hübsche Aufgaben für euch",
-    "Jetzt reißt euch mal am Riemen"
+    "Jetzt reißt euch mal am Riemen.",
+    "Da müsst ihr jetzt durch das Tal der Tränen.",
+    "Die Aufgabe 3 auf Seite 59 ist ne ganz hübsche.",
+    "PCHSCHSCHT!"
   ];
 
 function TextKeyUp (event)
@@ -31,7 +34,7 @@ function TextKeyUp (event)
   var username = localStorage.getItem ("username");
   var messages = JSON.parse (localStorage.getItem ("chatMessages"));
   messages[messages.length] = "<br/>" + username + ": " + text;
-  messages[messages.length] = "<br/>" + "Frau Mehr" + ": " + possibleMehrStuff[Math.floor (Math.random () * possibleMehrStuff.length)];
+  messages[messages.length] = "<br/>" + "Dr. Rhem" + ": " + possibleMehrStuff[Math.floor (Math.random () * possibleMehrStuff.length)];
   var stringToSave = JSON.stringify (messages);
   localStorage.setItem ("chatMessages", stringToSave);
   textBox.value = "";
